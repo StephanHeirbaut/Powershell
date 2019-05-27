a6d26beb6cda7786bc01e36a4f9635b97bc2280d
StephanHeirbaut <stephanheirbaut@hotmail.com>
2019-05-27 20:58:42 +0200

    documentatie aangepast

--- ExampleScripts/AzureAD.ps1
+++ ExampleScripts/AzureAD.ps1
@@ -2,9 +2,10 @@
 # Credentials to connect to AAD
 $credential = Get-Credential
 
-# Install Azure Active Directory module 
+# Install Azure Active Directory PS module
+# AzureAD nieuwere versie, maar moet nog uitgebreidt worden
+# MSOnline oudere versie, maar meer mogelijkheden
 Install-Module AzureAD
-
 Install-Module MSOnline 
 
 # Connect to AAD with MFA enabled
@@ -14,6 +15,8 @@ Connect-MsolService
 
 # Connect to AAD without MFA enabled
 Connect-AzureAD -Credential $credential
+
+Connect-MsolService -Credential $credential
 
 # Get all distributionlists and their members
 Get-ADGroup -Filter {GroupCategory -eq "Distribution"} -Properties Members
